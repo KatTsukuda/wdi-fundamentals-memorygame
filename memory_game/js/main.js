fc85b1058ccfd008d30de2f2e4e3cd7924f129f4
Kat Tsukuda <BearHeartsBat@Kats-MBP.mhs>
2017-05-06 11:52:35 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -12,15 +33,18 @@ var checkForMatch = function() {
 
 
 var flipCard = function(cardId){
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 if (cardsInPlay.length === 2) {
 	alert("Sorry, try again.");
 } else if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } 
-console.log("User flipped " + cards[cardId]);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+console.log("User flipped " + cards[cardId].rank);
 
 };
 flipCard(0);
 flipCard(2);
+
 
